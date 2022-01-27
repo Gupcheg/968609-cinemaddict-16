8f39e991c88261b7a6900aa7c4a642272029eff8
Gupcheg <gubarev_pasha_98@mail.ru>
2022-01-27 05:26:14 +0300

    Разделяй и властвуй

--- src/view/menu/site-menu-tpl.js
+++ src/view/menu/site-menu-tpl.js
@@ -1,47 +1,18 @@
-import {getFirstToUpperCase} from '../utils.js';
-import {createElement} from '../render.js';
+import { getFirstToUpperCase } from '../../utils/menu.js';
 
 const createMenuItemTemplate = (filter) => {
   const {name, count} = filter;
-
   return (
     `<a href="#${name}" class="main-navigation__item">${getFirstToUpperCase(name)} <span class="main-navigation__item-count">${count}</span></a>`
-
   );
 };
 
-const createSiteMenuTemplate = (filterItems) => {
+export const createSiteMenuTemplate = (filterItems) => {
   const filterItemsTemplate = filterItems
     .map((filter, index) => createMenuItemTemplate(filter, index === 0))
     .join('');
-
   return   `<div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
       ${filterItemsTemplate}
     </div>`;
 };
-
-export default class MenuView {
-  #element = null;
-  #filters = null;
-
-  constructor(filters) {
-    this.#filters = filters;
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  get template() {
-    return createSiteMenuTemplate(this.#filters);
-  }
-
-  removeElement() {
-    this.#element = null;
-  }
-}
