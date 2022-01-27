6fa0b0f8f6f7a8daa367cc0195ce8300042f23c4
Gupcheg <gubarev_pasha_98@mail.ru>
2022-01-27 21:40:08 +0300

    Большие перемены (часть 1 и 2)

---
+++ src/view/consts.js
@@ -0,0 +1,25 @@
+export const SortType = {
+  DEFAULT: 'default',
+  DATE: 'date',
+  RATING: 'rating',
+};
+
+export const FILM_BLOCKS = {
+  main: {
+    title: 'All movies. Upcoming',
+    isTitleHidden: true
+  },
+  topRated: {
+    title: 'Top rated',
+    isExtra: true
+  },
+  mostCommented: {
+    title: 'Most commented',
+    isExtra: true
+  },
+  empty: {
+    title: 'There are no movies in our database',
+    isEmpty: true
+  }
+};
+
